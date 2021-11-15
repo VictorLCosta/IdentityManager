763ac9b1e1bb5f1ab26f3cc3f1bc4d0d4b17160d
VictorLCosta <victorlc2019@outlook.com>
2021-11-15 20:55:02 -0300

    Project base

---
+++ IdentityManager.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdentityManager.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
